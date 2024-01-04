a81300c64f2f606584df3f66cacccbc2b02216f7
Dat Ngo <119279710+DatNgo-dev@users.noreply.github.com>
2024-01-04 22:02:48 +0800

    Added chi and cors

--- main.go
+++ main.go
@@ -3,17 +3,41 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load("env")
-	
-	port := os.Getenv("PORT")
-	if port == "" {
+	err := godotenv.Load("env")
+
+	if err != nil {
+		log.Fatal("Error: ", err)
+	} 
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
 		log.Fatal("PORT is not found in the environment")
 	}
-	fmt.Println("Port: ", port)
+	fmt.Println("PORT: ", PORT)
+	
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Handler: router,
+		Addr: ":" + PORT,
+	}
+
+	log.Printf("Server starting on port %v", PORT)
+	err = server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// router.Use(cors.Handler(cors.option{
+
+	// }))
 }
